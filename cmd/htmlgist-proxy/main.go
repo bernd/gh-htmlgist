778b360d92c35ea00af06195c94d1dce112b2bb9
Kay Roepke <kroepke@gmail.com>
2026-05-15 13:51:20 +0200

    feat: add proxy server binary

---
+++ cmd/htmlgist-proxy/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/kroepke/gh-htmlgist/internal/gist"
+	"github.com/kroepke/gh-htmlgist/internal/proxy"
+)
+
+type tokenTransport struct {
+	token   string
+	wrapped http.RoundTripper
+}
+
+func (t *tokenTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req = req.Clone(req.Context())
+	req.Header.Set("Authorization", "Bearer "+t.token)
+	return t.wrapped.RoundTrip(req)
+}
+
+func main() {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatal("GITHUB_TOKEN environment variable is required")
+	}
+
+	addr := os.Getenv("HTMLGIST_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	httpClient := &http.Client{
+		Transport: &tokenTransport{
+			token:   token,
+			wrapped: http.DefaultTransport,
+		},
+	}
+
+	client := gist.NewClient(httpClient)
+	handler := proxy.NewHandler(client)
+
+	log.Printf("listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, handler))
+}
